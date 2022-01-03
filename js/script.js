0099da191dbe77bf7c68cad116e4499720f67d32
Nathalia Rodrigues <nathrodrigues18@gmail.com>
2022-01-03 19:05:03 -0300

    Site Ada

---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
+
+function validaFormulario(){
+if(document.getElementById("nome").value != "" &&
+document.getElementById("telefone").value != "" ){
+    alert
+  }else{
+    alert("Por favor, preencha os campos nome e email.")
+  }
+}
+
+
+
+
